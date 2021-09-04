323c684731c9f20637466d2fd3af89b93dd40156
Ave M <your.email@example.com>
2021-09-04 14:18:00 -0400

    Add existing files

---
+++ index.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 24 10:21:47 2021
+
+@author: Avery
+"""
+
+import re
+
+
+path = '/Users/Avery/Downloads'
+
+x = re.findall("Lucki", path)
+print(x)
